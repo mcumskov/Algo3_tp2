b88cada28d1837787fde3c00a272c4382c9289ab
tabraham98 <103225591+tabraham98@users.noreply.github.com>
2023-11-18 13:41:12 -0300

    Cambio el nombre de la clase de prueba de dado para cunmplir con el regex

--- src/main/test/edu/fiuba/algo3/UnitTests/DadoUnitTests.java
+++ src/main/test/edu/fiuba/algo3/UnitTests/DadoUnitTests.java
@@ -4,7 +4,7 @@ import edu.fiuba.algo3.modelo.Dado;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class UnitTestsDado {
+public class DadoUnitTests {
 
     @Test
     public void test01LanzoUnDadoYElResultadoEstaEntre1y6(){
