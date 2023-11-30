e3ec1fea7f95ef4598c6ecc4402401bc2785e64e
tabraham98 <103225591+tabraham98@users.noreply.github.com>
2023-11-30 18:52:04 -0300

    Caso de uso 13

--- src/main/test/edu/fiuba/algo3/entrega_2/MessageTest2.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/MessageTest2.java
@@ -1,17 +1,28 @@
 package edu.fiuba.algo3.entrega_2;
 
 import edu.fiuba.algo3.modelo.Message;
+import edu.fiuba.algo3.modelo.excepciones.MapaJsonNoSePudoAbrirException;
+import edu.fiuba.algo3.modelo.mapa.Parser;
+import edu.fiuba.algo3.modelo.mapa.iCasilla;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MessageTest2 {
 
    @Test
-    public void test13intentoParsearMapaInvalidoReciboExcepcionMapaConClavesInvalidas(){
+    public void test13ParseoMapaReciboListaDeCasillasConTamanioCorrespondiente(){
 
+       String nombreArchivo = "src/main/test/edu/fiuba/algo3/jsonTests/mapa.json" ;
 
+       Parser parser = new Parser();
 
+       List<iCasilla> casillas = parser.parsearJSON(nombreArchivo);
 
+       assertEquals(39,casillas.size());
 
    }
 }
