af71901101a690c33d44fdb98d4d84859da98928
Galo12 <galoojeda3@gmail.com>
2023-12-01 10:55:13 -0300

    Cobertura LogFormatter

--- src/main/test/edu/fiuba/algo3/UnitTests/LogFormatterUnitTests.java
+++ src/main/test/edu/fiuba/algo3/UnitTests/LogFormatterUnitTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LogFormatterUnitTests {
 
     @Test
-    public void test01ElFormateadorAlRecibirElSiguienteRecordDebeDevolverElSiguienteString(){
+    public void test01ElFormateadorAlRecibirElSiguienteRecordConParametrosDebeDevolverElSiguienteString(){
         //Arrange
         LogFormatter formateador = new LogFormatter();
         LogRecord recordFalso = new LogRecord(Level.FINEST, "SoyUnMensaje");
@@ -20,6 +20,22 @@ public class LogFormatterUnitTests {
         recordFalso.setParameters(parametros);
 
         String mensajeEsperado = " [null] [FINEST]\u001B[37m - SoyUnMensaje\tint arg0, int arg1, char[] arg2, int arg3\u001B[0m\n";
+
+        //Act
+        String recordFormateado = formateador.format(recordFalso);
+        String[] recordFormateadoSinFecha = recordFormateado.split("\\]", 2);
+
+        //Assert
+        assertEquals(mensajeEsperado, recordFormateadoSinFecha[1]);
+    }
+
+    @Test
+    public void test02ElFormateadorAlRecibirElSiguienteRecordSinParametrosDebeDevolverElSiguienteString(){
+        //Arrange
+        LogFormatter formateador = new LogFormatter();
+        LogRecord recordFalso = new LogRecord(Level.FINEST, "SoyUnMensaje");
+
+        String mensajeEsperado = " [null] [FINEST]\u001B[37m - SoyUnMensaje\u001B[0m\n";
 
         //Act
         String recordFormateado = formateador.format(recordFalso);
