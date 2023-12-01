05e41a0b941b293f3fb70d9634b210ce12f96f5f
Galo12 <galoojeda3@gmail.com>
2023-12-01 10:51:30 -0300

    Agrego 100% de cobertura de clase LogFormatter

--- src/main/test/edu/fiuba/algo3/UnitTests/LogFormatterUnitTests.java
+++ src/main/test/edu/fiuba/algo3/UnitTests/LogFormatterUnitTests.java
@@ -2,6 +2,8 @@ package edu.fiuba.algo3.UnitTests;
 
 import edu.fiuba.algo3.modelo.log.LogFormatter;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,15 +12,20 @@ public class LogFormatterUnitTests {
 
     @Test
     public void test01ElFormateadorAlRecibirElSiguienteRecordDebeDevolverElSiguienteString(){
+        //Arrange
         LogFormatter formateador = new LogFormatter();
         LogRecord recordFalso = new LogRecord(Level.FINEST, "SoyUnMensaje");
+        Method metodo = String.class.getDeclaredMethods()[5];
+        Object[] parametros = metodo.getParameters();
+        recordFalso.setParameters(parametros);
 
+        String mensajeEsperado = " [null] [FINEST]\u001B[37m - SoyUnMensaje\tint arg0, int arg1, char[] arg2, int arg3\u001B[0m\n";
+
+        //Act
         String recordFormateado = formateador.format(recordFalso);
         String[] recordFormateadoSinFecha = recordFormateado.split("\\]", 2);
 
-
-        String mensajeEsperado = " [null] [FINEST]\u001B[37m - SoyUnMensaje\u001B[0m\n";
-
+        //Assert
         assertEquals(mensajeEsperado, recordFormateadoSinFecha[1]);
     }
 }
