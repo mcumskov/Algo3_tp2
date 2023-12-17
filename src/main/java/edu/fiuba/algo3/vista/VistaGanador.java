41789cc2baace0935c1e2c750d93388b5e6107d9
bubuli pedro <bubuli.pedro@gmail.com>
2023-12-17 20:15:20 -0300

    agrego fondos para las ultimas vistas

--- src/main/java/edu/fiuba/algo3/vista/VistaGanador.java
+++ src/main/java/edu/fiuba/algo3/vista/VistaGanador.java
@@ -4,9 +4,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import edu.fiuba.algo3.modelo.jugador.iJugador;
+
+import java.net.URL;
 
 public class VistaGanador {
     Stage mainStage;
@@ -19,12 +22,19 @@ public class VistaGanador {
         nombreGanador.getStyleClass().add("labelNombre");
         VBox box = new VBox(nombreGanador, botonSalir);
         box.setAlignment(Pos.CENTER);
-
+        box.setBackground(establecerFondoPantalla());
         Scene sceneInicio = new Scene(box, 300, 200);
         String cssFile = getClass().getResource("/style.css").toExternalForm();
         sceneInicio.getStylesheets().add(cssFile);
 
         mainStage.setScene(sceneInicio);
         mainStage.setTitle("GANADOR");
+    }
+
+    private Background establecerFondoPantalla() {
+        URL imageUrl = getClass().getResource("/image/casaPompeya.jpg");
+        Image backgroundImage = new Image(imageUrl.toExternalForm());
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true));
+        return new Background(background);
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/VistaPartidaPerdida.java
+++ src/main/java/edu/fiuba/algo3/vista/VistaPartidaPerdida.java
@@ -5,8 +5,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class VistaPartidaPerdida {
     Stage mainStage;
@@ -19,14 +22,21 @@ public class VistaPartidaPerdida {
         Label mensajeFinal = new Label("PERDIERON TODOS, SON MALISIMOS!!!\nGAME OVER\n");
         mensajeFinal.getStyleClass().add("labelNombre");
         VBox box = new VBox(mensajeFinal, botonSalir);
-        box.setAlignment(Pos.CENTER);
-
+        box.setAlignment(Pos.CENTER_LEFT);
+        box.setBackground(establecerFondoPantalla());
         Scene sceneInicio = new Scene(box, 300, 200);
         String cssFile = getClass().getResource("/style.css").toExternalForm();
         sceneInicio.getStylesheets().add(cssFile);
 
         mainStage.setScene(sceneInicio);
         mainStage.setTitle("GAME OVER");
+    }
+
+    private Background establecerFondoPantalla() {
+        URL imageUrl = getClass().getResource("/image/GameOver.jpg");
+        Image backgroundImage = new Image(imageUrl.toExternalForm());
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true));
+        return new Background(background);
     }
 
 }
